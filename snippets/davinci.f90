618a20426eeb5fd8720e938858e9e525fa970274
Owain Kenway <o.kenway@ucl.ac.uk>
2018-09-18 17:42:52 +0100

    davinci

---
+++ snippets/davinci.f90
@@ -0,0 +1,8 @@
+program davinci
+
+  implicit none
+
+  write(*,*) "_o_ Unless 5 million dollars are transferred into the following numbered" 
+  write(*,*) " |  account in 7 days I will capsize 5 tankers in the Ellingson fleet."
+  write(*,*) "/ \"
+end 
