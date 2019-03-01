b4658c3c295239be6095d095503684954b200134
Owain Kenway <o.kenway@ucl.ac.uk>
2019-03-01 11:35:02 +0000

    Matrix bench.

--- bench/microbench/README.md
+++ bench/microbench/README.md
@@ -5,6 +5,6 @@ Julia microbenchmarks fiddling
 
 The Julia project publish some microbenchmarks which include a benchmark called "pisum".  Since calculating Pi is one of muy hobbies, I was interested to see how it works.  It doesn't.  What it does is calculate 1.6448... 500 times.
 
-The `.py` and `.jl` files are exactly this routine, MIT licensed from the Julia project.  The `.cl` file is my implementation in Common LISP.
+The `microbench.py` and `microbench.jl` files are exactly this routine, MIT licensed from the Julia project.  The `microbench.cl` file is my implementation in Common LISP.
 
-![Benchmarks on my XPS13](https://pbs.twimg.com/media/DmQIYO3W0AIiZxa.png) 
+The `matrix.py` file is an implementation of the two Numpy based benchmarks.
---
+++ bench/microbench/matrix.py
@@ -0,0 +1,43 @@
+from numpy import *
+from numpy.random import rand, randn
+from numpy.linalg import matrix_power
+
+def matrix_multiply(n):
+    a = rand(n,n)
+    b = rand(n,n)
+    return dot(a,b)
+
+
+def matrix_stats(t):
+    n = 5
+    v = zeros(t)
+    w = zeros(t)
+
+    for i in range(t):
+        a = rand(n,n)
+        b = rand(n,n)
+        c = rand(n,n)
+        d = rand(n,n) 
+
+        p = concatenate((a,b,c,d), axis=1)
+        q = concatenate((concatenate((a,b),axis=1), cocatenate((c,d), axis=1)), axis=0)
+
+        v[i] = trace(matrix_power(dot(p,t,p),4))
+        w[i] = trace(matrix_power(dot(q,t,q),4))
+
+    return (std(v)/mean(v), std(w)/mean(w))
+
+
+def bench():
+	import time
+	start1 = time.time()
+	c=matrix_multiply(1000)
+	stop1 = time.time()
+
+    start2 = time.time()
+	(d,e)=matrix_stats(1000)
+	stop2 = time.time()
+	return [(stop1 - start1), (stop2 - start2)]
+
+if __name__=="__main__":
+	print(bench())
