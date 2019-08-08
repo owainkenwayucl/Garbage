27101bf3a8d8aa0351e3c4794ccdc4fcd57ea7e2
Owain Kenway <o.kenway@ucl.ac.uk>
2019-08-08 11:34:56 +0100

    Added Easycargo Heatsink data series.

--- snippets/pi_temp/plot.R
+++ snippets/pi_temp/plot.R
@@ -1,7 +1,11 @@
-temps = read.csv("temp.dat")
-
+temps_g <- read.csv("temp_gorillapi.dat")
+temps_e <- read.csv("temp_easycargo_20mm.dat")
 png(file="temperature.png", width=1024, height=720)
 
-plot(temps$Temperature, xlab="Minutes since login", ylab="Temperature (°C)", main="Temperature of idling Raspberry Pi 4")
 
+colours <- c(2,4)
+plot(0,0,xlim=c(0,120), ylim=c(30,70), type="n", xlab="Minutes since login", ylab="Temperature (°C)", main="Temperature of idling Raspberry Pi 4")
+lines(temps_g$Temperature, col = colours[1], type="b")
+lines(temps_e$Temperature, col = colours[2], type="b")
+legend("topleft", legend=c("GorlliaPi", "EasyCargo 20mm"), col = colours, pch=1)
 dev.off()
