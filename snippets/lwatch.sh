de09d94325ab273f3a63bafda214838c1e0ef1d5
Owain Kenway <o.kenway@ucl.ac.uk>
2020-12-04 17:29:13 +0000

    Terrible df checker.

---
+++ snippets/lwatch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+delay=${delay:-2}
+scale=${scale:-2}
+block=${block:-M}
+x=`df -B${block} | grep lustre | awk '{print $3}'`
+
+while [ true ]
+do
+sleep $delay
+y=$x
+x=`df -B${block} | grep lustre | awk '{print $3}'`
+z=`echo "scale=$scale; ((${x%${block}}-${y%${block}})/$delay)" | bc`
+echo delta $z ${block}/s
+done
