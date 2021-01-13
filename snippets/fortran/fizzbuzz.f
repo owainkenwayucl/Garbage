60f6b3a6276a71e028417a4f6eaa925dc400f6f9
Owain Kenway <o.kenway@ucl.ac.uk>
2021-01-13 09:57:00 +0000

    Compted goto fizzbuzz

---
+++ snippets/fortran/fizzbuzz.f
@@ -0,0 +1,18 @@
+	program fizzbuzz
+	implicit none
+	integer i,j,l
+	do i=1,20
+	j=0
+	if (mod(i,3) .eq. 0) j = j + 1
+	if (mod(i,5) .eq. 0) j = j + 2
+	goto (10,20,30),j
+	write(*,*) i
+	goto 100
+10	write(*,*) "       Fizz"
+	goto 100
+20	write(*,*) "       Buzz"
+	goto 100
+30	write(*,*) "   Fizzbuzz"
+100	continue
+	enddo
+	end program 
