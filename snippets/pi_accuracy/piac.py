7629a24a296b058d7ff18872dbd16a0a264edaa2
Owain Kenway <o.kenway@ucl.ac.uk>
2022-09-08 15:44:48 +0100

    Use numba

--- snippets/pi_accuracy/piac.py
+++ snippets/pi_accuracy/piac.py
@@ -2,22 +2,28 @@ import sys
 import time
 import math
 import random
+import numba
 
+@numba.jit(parallel=True, nopython=True)
 def integralpi(n, m):
 	numsamples = n*m
 	totalsum = 0
 	step = 1.0/numsamples
 
-	p = step * sum(4.0/(1.0 + ((i + 0.5) * (i + 0.5) * step * step)) for i in range(numsamples))
+#	p = step * sum(4.0/(1.0 + ((i + 0.5) * (i + 0.5) * step * step)) for i in range(numsamples))
+	for i in numba.prange(numsamples):
+		x = (i + 0.5) * step
+		totalsum += 4.0/(1.0 + (x * x))
 
+	p = totalsum * step
 	return p, abs(p - math.pi)
 	
-
+@numba.jit(parallel=True, nopython=True)
 def montecarlopi(n, m):
 	numsamples = n*m
 
 	count = 0
-	for a in range(numsamples):
+	for a in numba.prange(numsamples):
 		x = random.random()
 		y = random.random()
 		if ((x*x + y*y) <= 1):
@@ -26,14 +32,14 @@ def montecarlopi(n, m):
 	p = 4*count/numsamples
 	return p, abs(p - math.pi)
 
-
+@numba.jit(parallel=True, nopython=True)
 def gridpi(n, m):
 	p = math.pi
 	xstep = 1.0/n
 	ystep = 1.0/m
 
 	count = 0
-	for a in range(n):
+	for a in numba.prange(n):
 		for b in range(m):
 			x = 0.0 + (a * xstep)
 			y = 0.0 + (b * ystep)
@@ -55,6 +61,11 @@ def timefunc(function, n, m):
 def bench(lower, step, upper):
 	current = lower
 
+	# precompile
+	tt, tp, te = timefunc(integralpi, 1, 1)
+	tt, tp, te = timefunc(montecarlopi, 1, 1)
+	tt, tp, te = timefunc(gridpi, 1, 1)
+
 	print("Samples, Integral time, Integral error, Monte Carlo time, Monte Carlo error, Grid time, Grid error")
 	while (current < upper):
 		dim = math.floor(math.sqrt(current))
@@ -70,6 +81,8 @@ if __name__ == '__main__':
 	begin = 1000
 	stop = 10000
 	step = 100
+
+	# precompile step
 
 	if len(sys.argv) > 1:
 		begin = int(sys.argv[1])
