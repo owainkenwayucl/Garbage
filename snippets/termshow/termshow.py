c3ca9bf78f3af7f5a45520dd2e26f81fb010bfe1
Owain Kenway <o.kenway@ucl.ac.uk>
2022-11-02 21:26:05 +0000

    more hacks

--- snippets/termshow/termshow.py
+++ snippets/termshow/termshow.py
@@ -2,16 +2,13 @@ COLOURS=" ░▒▓█"
 
 def show(data, x, y, colours=COLOURS):
 	num_colours=len(colours)
-	mx = max(data)
-	mn = min(data)
-	rn = mx - mn
 	for a in range(y):
 		for b in range(x):
-			v = (data[a][b] - mn)/rn
+			v = data[a][b]
 				
 			quantised = int(v * num_colours)
 			if quantised >= len(colours):
-				quantised = len(colours - 1)
+				quantised = len(colours) - 1
 
 			print(colours[quantised], end='')
 		print("")
