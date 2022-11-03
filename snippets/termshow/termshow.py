a9ee916e2633c638df855c6154da964c7c8a9b9e
Owain Kenway <o.kenway@ucl.ac.uk>
2022-11-03 09:56:00 +0000

    Nicer implementation
    ASCII colours as per https://stackoverflow.com/a/67780964/12304143

--- snippets/termshow/termshow.py
+++ snippets/termshow/termshow.py
@@ -1,14 +1,17 @@
-COLOURS=" ░▒▓█"
+COLOURS=' ░▒▓█'
+ASCII_COLOURS='.\'`^",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
 
-def show(data, x, y, colours=COLOURS):
+def show(data, colours=COLOURS):
+	height = len(data)
 	num_colours=len(colours)
-	for a in range(y):
-		for b in range(x):
+	for a in range(height):
+		width = len(a)
+		for b in range(width):
 			v = data[a][b]
 				
-			quantised = int(v * num_colours)
-			if quantised >= len(colours):
-				quantised = len(colours) - 1
+			quantised = int(v * (num_colours-1))
+#			if quantised >= len(colours):
+#`				quantised = len(colours) - 1
 
 			print(colours[quantised], end='')
 		print("")
