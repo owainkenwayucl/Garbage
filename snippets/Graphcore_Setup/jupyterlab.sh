99bd78326b6589a5fc06f36ff132313263202a65
Owain Kenway <o.kenway@ucl.ac.uk>
2023-09-14 12:40:21 +0100

    Add Jupyterlab configs

--- snippets/Graphcore_Setup/jupyterlab.sh
+++ snippets/Graphcore_Setup/jupyterlab.sh
@@ -1,3 +1,4 @@
 source ~/Setup/poplar.sh
+export JUPYTERLAB_DIR=${HOME}/Applications/Jupyter/jupyterlab
 export PATH=${HOME}/Applications/Jupyter/bin:${PATH}
 export PYTHONPATH=${HOME}/Applications/Jupyter:${PYTHONPATH}
