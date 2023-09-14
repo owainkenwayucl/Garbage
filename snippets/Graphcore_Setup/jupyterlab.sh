07e5943cc30d92e7e9e584813a20e2cb5d58f1c2
Owain Kenway <o.kenway@ucl.ac.uk>
2023-09-14 13:44:18 +0100

    We're doing Jupyter differently.

--- snippets/Graphcore_Setup/jupyterlab.sh
+++
@@ -1,4 +0,0 @@
-source ~/Setup/poplar.sh
-export JUPYTERLAB_DIR=${HOME}/Applications/Jupyter/jupyterlab
-export PATH=${HOME}/Applications/Jupyter/bin:${PATH}
-export PYTHONPATH=${HOME}/Applications/Jupyter:${PYTHONPATH}
