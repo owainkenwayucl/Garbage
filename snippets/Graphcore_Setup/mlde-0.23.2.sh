948396dc4a36477d08bd301abb7063b5b8d61832
Owain Kenway <o.kenway@ucl.ac.uk>
2023-10-09 10:35:51 +0100

    Typos

--- snippets/Graphcore_Setup/mlde-0.23.2.sh
+++ snippets/Graphcore_Setup/mlde-0.23.2.sh
@@ -1,3 +1,3 @@
-source ~/Setup/rhel-tools
-source ~/Applications/MLDEW/mlde-cli-0.23.2/bin/activate
+source ~/Setup/rhel-tools.sh
+source ~/Applications/MLDE/mlde-cli-0.23.2/bin/activate
 export DET_MASTER=https://mlde.conjurer.arc.ucl.ac.uk
