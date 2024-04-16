4da8e3eb3e3d6adc6277e54a9a850c7486644a82
Owain Kenway <o.kenway@ucl.ac.uk>
2024-04-16 15:34:36 +0100

    Qualcomm config scripts

---
+++ snippets/Qualcomm_Setup/python_3.8.18.sh
@@ -0,0 +1,4 @@
+_py_home=/home/uccaoke/Applications/python-3.8.18
+
+export PATH=${_py_home}/bin:${PATH}
+export LD_LIBRARY_PATH=${_py_home}/lib:$LD_LIBRARY_PATH
---
+++ snippets/Qualcomm_Setup/qaic_python.sh
@@ -0,0 +1,5 @@
+source /opt/qti-aic/cloud-ai100-enable.sh
+
+source ${HOME}/Setup/python_3.8.18.sh
+
+source ${HOME}/Applications/qaic-python3.8.18/bin/activate
