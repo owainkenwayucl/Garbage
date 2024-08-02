614f35b89d5e44e740cc98cff56ec2a59ae939e2
Owain Kenway <o.kenway@ucl.ac.uk>
2024-08-02 14:10:07 +0100

    Python script to remove erroneous CA info from Rancher kubeconfig

---
+++ snippets/unfuck_rancher_kubeconfig.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+settings_file = f"{os.path.expanduser('~')}/.rancher/cli2.json"
+
+print(f"Updating Settings file {settings_file}")
+
+data = ""
+
+with open(settings_file) as f_handle:
+    data = json.load(f_handle)
+
+kube_confs = list(data['Servers']['rancherDefault']['kubeConfigs'].keys())
+
+for a in kube_confs:
+    clusters = list(data['Servers']['rancherDefault']['kubeConfigs'][a]['clusters'].keys())
+    for b in clusters:
+		print(f"Updating {a}/{b}")
+        temp = data['Servers']['rancherDefault']['kubeConfigs'][a]['clusters'][b].pop('certificate-authority-data', None)
+            if not temp == None:
+                print(f">>> CA information for {a}\{b} already deleted.")
+
+json_out = json.dumps(data, indent=4)
+
+print(json_out)
+
+with open(settings_file, "w") as f_handle:
+    f_handle.write(json_out)
