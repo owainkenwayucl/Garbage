1a94e0c34b831424e76f81d9277a716094e3b66b
Owain Kenway <o.kenway@ucl.ac.uk>
2024-12-02 16:06:00 +0000

    Add poplar 3.4.0

---
+++ snippets/Graphcore_Setup/poplar_3.4.0.sh
@@ -0,0 +1,3 @@
+source scl_source enable gcc-toolset-9
+source /opt/graphcore/poplar_sdk-3.4.0/poplar-rhel_8-3.4.0+73-aa67dd6164/enable.sh
+source /opt/graphcore/poplar_sdk-3.4.0/popart-rhel_8-3.4.0+73-aa67dd6164/enable.sh
